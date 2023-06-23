9c251c96542c715683647d0c6901914d20b21f8b
eliashenriksen <79273139+eliashenriksen@users.noreply.github.com>
2023-06-23 13:22:46 +0200

    Update HomeCanvas2.jsx

--- components/threejs/homecanvas2/HomeCanvas2.jsx
+++ components/threejs/homecanvas2/HomeCanvas2.jsx
@@ -6,6 +6,7 @@ import { Stats } from "@react-three/drei";
 
 export default function HomeCanvas2() {
 
+  //Change "dpr" for performance boost on mobile, lower dpr = lower render quality. Device pixel ratio.
   return(
     <Canvas shadows camera={{ fov: 50, near: 0.1, far: 2000, position: [0, 0, -5] }} dpr={[0.1, 0.6]}>
       <Lights></Lights>
